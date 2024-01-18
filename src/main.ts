b72ac55e844d1348c25ffd48bc6b02c86740c31b
ipunganwar <ipung.anwar1@gmail.com>
2024-01-18 12:56:07 +0700

    feat: config main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
 import './style.css'
 import router from './router'
 import App from './App.vue'
 
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const vuetify = createVuetify({
+  components,
+})
+
 const app = createApp(App)
+app.use(pinia)
+app.use(vuetify)
 app.use(router)
 app.mount('#app')
